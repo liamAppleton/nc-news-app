2b8f6c507781a3fe9682bd1972cec6760737659e
Liam Appleton <liamappleton97@outlook.com>
2025-03-12 14:43:10 +0000

    refactoring articles router

--- routes/articles.router.js
+++ routes/articles.router.js
@@ -9,9 +9,15 @@ const {
 const articlesRouter = require('express').Router();
 
 articlesRouter.route('/').get(getArticles);
-articlesRouter.route('/:article_id').get(getArticleById);
-articlesRouter.route('/:article_id/comments').get(getCommentsByArticleId);
-articlesRouter.route('/:article_id/comments').post(postCommentByArticleId);
-articlesRouter.route('/:article_id').patch(patchArticleById);
+
+articlesRouter
+  .route('/:article_id')
+  .get(getArticleById)
+  .patch(patchArticleById);
+articlesRouter
+
+  .route('/:article_id/comments')
+  .get(getCommentsByArticleId)
+  .post(postCommentByArticleId);
 
 module.exports = articlesRouter;

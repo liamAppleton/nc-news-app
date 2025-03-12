326060da15ae64a1609ca9b5a59219f016ec2c7b
Liam Appleton <liamappleton97@outlook.com>
2025-03-12 09:56:27 +0000

    preparing files for hosting

--- listen.js
+++ listen.js
@@ -1,6 +1,4 @@
 const app = require('./app.js');
+const { PORT = 9000 } = process.env;
 
-app.listen(9000, (err) => {
-  if (err) console.log(err);
-  else console.log('Listening on port 9000...');
-});
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));

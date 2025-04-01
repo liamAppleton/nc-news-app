9d927bdf1a65a3a6df1b8765163466b71f96abcc
Liam Appleton <liamappleton97@outlook.com>
2025-04-01 12:17:12 +0100

    adding COALESCE to article path controller to account for votes being null

--- models/articles.models.js
+++ models/articles.models.js
@@ -146,7 +146,7 @@ const updateArticleById = ({ inc_votes, article_id }) => {
   return db
     .query(
       `UPDATE articles
-    SET votes = votes + $1
+    SET votes = COALESCE(votes, 0) + $1
     WHERE article_id = $2
     RETURNING *`,
       [inc_votes, article_id]

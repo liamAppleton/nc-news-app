af88e60fb8e98b1b71668de6fc80765d3302e42e
Liam Appleton <liamappleton97@outlook.com>
2025-04-04 14:08:06 +0100

    removing quotes around booleans in test data

--- db/data/test-data/commentLikes.js
+++ db/data/test-data/commentLikes.js
@@ -1,8 +1,8 @@
 module.exports = [
-  { username: 'butter_bridge', comment_id: 1, liked: 'true' },
-  { username: 'butter_bridge', comment_id: 2, liked: 'false' },
-  { username: 'butter_bridge', comment_id: 3, liked: 'true' },
-  { username: 'icellusedkars', comment_id: 1, liked: 'true' },
-  { username: 'icellusedkars', comment_id: 5, liked: 'false' },
-  { username: 'icellusedkars', comment_id: 6, liked: 'true' },
+  { username: 'butter_bridge', comment_id: 1, liked: true },
+  { username: 'butter_bridge', comment_id: 2, liked: false },
+  { username: 'butter_bridge', comment_id: 3, liked: true },
+  { username: 'icellusedkars', comment_id: 1, liked: true },
+  { username: 'icellusedkars', comment_id: 5, liked: false },
+  { username: 'icellusedkars', comment_id: 6, liked: true },
 ];
